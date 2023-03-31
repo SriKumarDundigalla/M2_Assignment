4f71a5b7917d3e937fad29e781a40c875e901936
SriKumarDundigalla <111296560+SriKumarDundigalla@users.noreply.github.com>
2023-03-31 17:14:58 -0400

    Delete job.sh

--- job.sh
+++
@@ -1,6 +0,0 @@
-/usr/bin/hadoop jar /usr/lib/hadoop-mapreduce/hadoop-streaming.jar 
-  -files mapper.py, reducer.py
-  -mapper mapper.py 
-  -reducer reducer.py 
-  -input /mapreduce/test1.txt 
-  -output /mapreduce/output1
