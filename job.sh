ef83af68b4bf1bbb47367fc8c7590cf9d1640b7c
SriKumarDundigalla <111296560+SriKumarDundigalla@users.noreply.github.com>
2023-04-01 17:16:05 -0400

    Add files via upload

---
+++ job.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+/usr/bin/hadoop jar /usr/lib/hadoop-mapreduce/hadoop-streaming.jar -files mapper.py, reducer.py -mapper "python mapper.py" -reducer "python reducer.py" -input /mapreduce/test.txt  -output /mapreduce/output
+
+
--- reducer.py
+++ reducer.py
@@ -4,9 +4,8 @@
 from operator import itemgetter
 import sys
 
-current_word = None
-current_count = 0
-word = None
+total=0
+count_tot=0
 
 # input comes from STDIN
 for line in sys.stdin:
@@ -14,27 +13,18 @@ for line in sys.stdin:
     line = line.strip()
 
     # parse the input we got from mapper.py
-    word, count = line.split('\t', 1)
+    num,count = line.split('\t', 1)
 
     # convert count (currently a string) to int
     try:
+	    num=int(num)
         count = int(count)
     except ValueError:
         # count was not a number, so silently
         # ignore/discard this line
         continue
 
-    # this IF-switch only works because Hadoop sorts map output
-    # by key (here: word) before it is passed to the reducer
-    if current_word == word:
-        current_count += count
-    else:
-        if current_word:
-            # write result to STDOUT
-            print '%s\t%s' % (current_word, current_count)
-        current_count = count
-        current_word = word
-
-# do not forget to output the last word if needed!
-if current_word == word:
-    print '%s\t%s' % (current_word, current_count)
+    total=num+total
+    count_tot=count+count_tot
+if count >0:
+   print 'avg:%s'%(total/float(count_tot))
