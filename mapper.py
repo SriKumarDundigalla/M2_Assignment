4b58acab03da97a2fb54b2dabfbbe8e63608f569
SriKumarDundigalla <111296560+SriKumarDundigalla@users.noreply.github.com>
2023-03-31 21:52:58 -0400

    Add files via upload

---
+++ mapper.py
@@ -0,0 +1,15 @@
+import sys
+
+for line in sys.stdin:
+    sum_numbers=0
+    count=0
+    line = line.strip()
+
+    numbers=line.split()
+
+    for num in numbers:
+        sum_numbers = int(num)+sum_numbers
+        count = count+1
+
+    print '%s\t%s' % (count,sum_numbers)
+
