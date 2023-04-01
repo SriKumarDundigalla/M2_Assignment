a3efd7b80e6563fb33bd2e42e8d678d66dc3abf5
SriKumarDundigalla <111296560+SriKumarDundigalla@users.noreply.github.com>
2023-03-31 21:49:56 -0400

    Add files via upload

--- mapper.py
+++ mapper.py
@@ -1,20 +1,15 @@
-#!/usr/bin/env python
-"""mapper.py"""
-
 import sys
-
-# input comes from STDIN (standard input)
+sum_numbers=0
+count=0
 for line in sys.stdin:
-    # remove leading and trailing whitespace
+   
     line = line.strip()
-    # split the line into words
-    words = line.split()
-    # increase counters
-    for word in words:
-        # write the results to STDOUT (standard output);
-        # what we output here will be the input for the
-        # Reduce step, i.e. the input for reducer.py
-        #
-        # tab-delimited; the trivial word count is 1
-        print '%s\t%s' % (word, 1)
+
+    numbers=line.split()
+
+    for num in numbers:
+        sum_numbers = int(num)+num
+        count = count+1
+
+    print '%s\t%s' % (count,sum_numbers)
 
